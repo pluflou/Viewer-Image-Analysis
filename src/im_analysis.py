ea8aff9da958a8b21c71e7b26e4a7f6ed59a43ea
Sara <pluflou@gmail.com>
2019-03-19 10:50:09 -0400

    updated axes, imshow colormap

--- src/im_analysis.py
+++ src/im_analysis.py
@@ -45,6 +45,7 @@ mpl.style.use('seaborn')
 fig = plt.figure(figsize=(7, 5))
 grid = plt.GridSpec(5, 7, hspace=0, wspace=0)
 main_ax = fig.add_subplot(grid[:-1, 1:])
+
 #main_ax.get_xaxis().set_visible(False)
 plt.setp(main_ax.get_xticklabels(), fontsize=0)
 plt.setp(main_ax.get_yticklabels(), fontsize=0)
@@ -53,11 +54,14 @@ y_hist = fig.add_subplot(grid[:-1, 1], sharey=main_ax)
 x_hist = fig.add_subplot(grid[-1, 2:6], sharex=main_ax)
 
 #Plot the reduced image in center 
-main_ax.imshow(image.subtracted_data)
+im=main_ax.imshow(image.subtracted_data,  cmap='cividis')
+#cax = fig.add_axes([04.27, 0.8, 4.5, 0.05])
+#fig.colorbar(im, cax=cax, orientation='vertical')
+main_ax.grid(False)
 try:
-	main_ax.set_title("Image: "+sys.argv[1]+ "\nScale: %.1f mm is 1 pixel\nCenter X-pos: %.1f +/- %.1f mm, Center Y-pos: %.1f +/- %.1f mm" %(px_to_mm, beam_location[4], abs(x_peak_idx-x_med)*px_to_mm, beam_location[5], abs(y_peak_idx-y_med)*px_to_mm), fontsize=12)
+	main_ax.set_title("Image: "+sys.argv[1]+ "\nScale: %.1f mm is 1 pixel\nCenter X-pos: %.1f +/- %.1f mm, Center Y-pos: %.1f +/- %.1f mm" %(px_to_mm, beam_location[4], abs(x_peak_idx-x_med)*px_to_mm, beam_location[5], abs(y_peak_idx-y_med)*px_to_mm), fontsize=11)
 except NameError:
-	main_ax.set_title("Image: "+sys.argv[1]+ "\nScale: Not found\nRaw Center X-pos: %.1f +/- %.1f px, Center Y-pos: %.1f +/- %.1f px" %(abs(x_med), abs(x_peak_idx-x_med), abs(y_med), abs(y_peak_idx-y_med)), fontsize=12)
+	main_ax.set_title("Image: "+sys.argv[1]+ "\nScale: Not found\nRaw Center X-pos: %.1f +/- %.1f px, Center Y-pos: %.1f +/- %.1f px" %(abs(x_med), abs(x_peak_idx-x_med), abs(y_med), abs(y_peak_idx-y_med)), fontsize=11)
 	
 main_ax.plot([0, image.shape[1]], [y_med,y_med], linewidth=0.6, color='r')
 main_ax.plot( [x_med, x_med], [0, image.shape[0]], linewidth=0.6, color='r')
@@ -70,19 +74,22 @@ x_hist.plot(x, image.profile_x)
 x_hist.plot([x_med,x_med],[0, x_peak],  linewidth=0.6, color='r', marker='.', markersize=4, label="Median")
 x_hist.plot([x_peak_idx,x_peak_idx],[0, x_peak],  linewidth=0.6, color='gray', linestyle='-', label="Peak")
 x_hist.legend(loc="upper right", prop={'size':8})
-x_hist.xaxis.set_minor_locator(AutoMinorLocator())
+xticks=[0,15,30,45,60,75,90,105,120,135,150,165]
+x_hist.set_xticks(xticks)
+plt.xticks(rotation=30)
 
 
 y_hist.plot(image.profile_y, y)
 y_hist.plot([0, y_peak], [y_med, y_med], linewidth=0.6, color='r', marker='.', markersize=4, label="Median")
 y_hist.plot([0, y_peak], [y_peak_idx, y_peak_idx], linewidth=0.6, color='gray',  linestyle='-', label="Peak")
-y_hist.minorticks_on()
+yticks=[0,15,30,45,60,75,90,105,120,135,150,165]
+y_hist.set_yticks(yticks)
 y_hist.invert_xaxis()
 
 
 timestring = (datetime.datetime.now()).strftime("%m-%d_%H:%M.%f")
 image_name='D1542'+'_'+timestring+'.png'
-#plt.savefig('../output/'+image_name, dpi=300)
+plt.savefig('../output/'+image_name, dpi=300)
 #np.savetxt('Loc_'+image_name+'.csv', [beam_location], header="X-Median (px), Y-Median (px), X-Peak (px), Y-Peak (px), X-Location (mm), Y-Location (mm)")
 plt.show()
  
