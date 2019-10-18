changing from median to mean calculation to find center. Not final, still trying to fix the bg subtraction problem

src/im_analysis.py
@@ -6,7 +6,7 @@ import warnings
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-#import scipy.signal as signal
+from scipy import stats
 #from lmfit import Model
 #from lmfit.models import GaussianModel, SkewedGaussianModel, LorentzianModel, VoigtModel
 from scipy.signal import savgol_filter
@@ -22,13 +22,29 @@ image.subtract_bg(bg)
 image.get_profile()
 
 #Apply a smoothing Savitzky-Golay filter to the arrays: applies a polynomial of order 3 for every 5 data points
-y_smooth=savgol_filter(image.profile_y, 5, 3)
-x_smooth=savgol_filter(image.profile_x, 5, 3)
+#y_smooth=savgol_filter(image.profile_y, 5, 3)
+#x_smooth=savgol_filter(image.profile_x, 5, 3)
 
-#Fit the profiles to selected models and find the point where the median of the data lays
-#x_med, x_sigp, x_sign =models[x_profile_model](x_smooth,x)
-#y_med, y_sigp, y_sign =models[y_profile_model](y_smooth,y)
+y = np.arange(image.y_size)
+x = np.arange(image.x_size)
+#print(repr(x), repr(y))
+#print(repr(image.profile_x), repr(image.profile_y))
 
+#bin the  data
+##################################REMOVE THE BIN_F FACTOR AND CHANGE DDOF################################################################
+#x_smooth, x, xbinnumber = stats.binned_statistic(x, image.profile_x, 'sum', bins=len(x)/bin_f)
+#y_smooth, y, ybinnumber = stats.binned_statistic(y, image.profile_y, 'sum', bins=len(y)/bin_f)
+x_smooth = image.profile_x
+y_smooth = image.profile_y
+
+#Find the mean and sigma of the data
+x_mean = np.average(x, weights = x_smooth, axis=0)
+y_mean = np.average(y, weights = y_smooth, axis=0)
+
+
+x_std = 1#np.std(x_smooth/bin_f, ddof = 0)
+y_std = 1#np.std(y_smooth/bin_f, ddof = 0)
+#'''
 #UPDATE 2/12/19:
 #instead of fitting with a model, take the median. Comparing to actual peak can give an idea of error.
 x_med, x_sigp, x_sign = findMedian(x_smooth[220:300])
@@ -43,8 +59,6 @@ x_loc, y_loc= (x_med-d1542_center[0]), (d1542_center[1]-y_med) #y-axis is invert
 beam_location=[x_med, y_med, x_peak_idx, y_peak_idx, x_loc*px_to_mm, y_loc*px_to_mm]
 
 #### plot and save results ####
-y = np.arange(image.y_size)
-x = np.arange(image.x_size)
 
 mpl.style.use('seaborn')
 fig = plt.figure(figsize=(16, 8))
@@ -70,22 +84,23 @@ main_ax.grid(False)
 
 try:
 	main_ax.set_title(
-		"Center X-pos: %.1f +/- %.1f mm, Center Y-pos: %.1f +/- %.1f mm" 
-		%(beam_location[4], abs(x_peak_idx-x_med)*px_to_mm, beam_location[5], abs(y_peak_idx-y_med)*px_to_mm),
+		"Image: "+sys.argv[1][82:-5]+
+		"\nCenter X-pos: %.1f +/- %.1f mm, Center Y-pos: %.1f +/- %.1f mm" 
+		%(beam_location[4], abs(x_peak_idx-x_mean)*px_to_mm, beam_location[5], abs(y_peak_idx-y_mean)*px_to_mm),
 		fontsize=11
 		)
 except NameError:
 	main_ax.set_title(
 		"Image: "+sys.argv[1]+
 		"\nScale: Not found\nRaw Center X-pos: %.1f +/- %.1f px, Center Y-pos: %.1f +/- %.1f px"
-		%(abs(x_med), abs(x_peak_idx-x_med), abs(y_med), abs(y_peak_idx-y_med)), 
+		%(abs(x_mean), abs(x_peak_idx-x_mean), abs(y_mean), abs(y_peak_idx-y_mean)), 
 		fontsize=11
 		)
 	
-main_ax.plot([0, image.shape[1]], [y_med,y_med],
+main_ax.plot([0, image.shape[1]], [y_mean,y_mean],
             linewidth=0.6, color='r'
 			)
-main_ax.plot( [x_med, x_med], [0, image.shape[0]],
+main_ax.plot( [x_mean, x_mean], [0, image.shape[0]],
             linewidth=0.6, color='r'
 			)
 
@@ -97,38 +112,43 @@ main_ax.plot(d1542_dots[0][:], d1542_dots[1][:],
 # plot the x and y profiles
 #orange=(200/255,82/255,0/255)
 x_hist.plot(x, image.profile_x)
-x_hist.plot([x_med,x_med],[0, x_peak],
-            linewidth=0.6, color='r', marker='.', markersize=4, label="Median"
+x_hist.plot([x_mean,x_mean],[0, x_peak],
+            linewidth=0.6, color='r', marker='.', markersize=4, label="Mean"
 			)
 x_hist.plot([x_peak_idx,x_peak_idx],[0, x_peak],
             linewidth=0.6, color='gray', linestyle='-', label="Peak"
 			)
 x_hist.legend(loc="upper right", prop={'size':8})
-xticks=[i for i in range(image.x_size) if i%20==0]
+xticks=[i for i in range(x.max()) if i%20==0]
 x_hist.set_xticks(xticks)
 plt.xticks(rotation=30)
 
 y_hist.plot(image.profile_y, y)
-y_hist.plot([0, y_peak], [y_med, y_med], 
-            linewidth=0.6, color='r', marker='.', markersize=4, label="Median"
+y_hist.plot([0, y_peak], [y_mean, y_mean], 
+            linewidth=0.6, color='r', marker='.', markersize=4, label="Mean"
 			)
 y_hist.plot([0, y_peak], [y_peak_idx, y_peak_idx],
             linewidth=0.6, color='gray',  linestyle='-', label="Peak"
 			)
-yticks= [i for i in range(image.y_size) if i%20==0]
+yticks= [i for i in range(y.max()) if i%20==0]
 y_hist.set_yticks(yticks)
 y_hist.invert_xaxis()
 
 #check that paths exists/create it
 mkdir_p(output_path)
-
+#'''
 #save results
+
+f= open(f"data_mean_stdev_fullarray_{bin_f}pix.txt", "a+")
+f.write(f'{sys.argv[1][82:-5]} {x_mean} {x_std} {y_mean} {y_std}\n')
+f.close()
+
 timestring = (datetime.datetime.now()).strftime("%m-%d_%H:%M.%f")
 image_name= viewer_loc + '_' + timestring
 plt.savefig(output_path + 'ViewerCenter' + image_name + '.png', dpi=300)
 #np.savetxt(output_path + 'BeamLoc_' + image_name + '.csv',
 #          [beam_location], 
-#          header="X-Median (px), Y-Median (px), X-Peak (px), Y-Peak (px), X-Location (mm), Y-Location (mm)"
+#          header="X-Mean (px), Y-Mean (px), X-Peak (px), Y-Peak (px), X-Location (mm), Y-Location (mm)"
 # )
 
 if (show_plots== True):