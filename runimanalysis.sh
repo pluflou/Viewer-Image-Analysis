5fbce9b0055e31371fc152e0712117e38daa8d82
Sara <pluflou@gmail.com>
2020-01-28 18:12:12 -0500

    minor edits to run file

--- runimanalysis.sh
+++ runimanalysis.sh
@@ -1,4 +1,4 @@
-for file in /home/sara/Documents/SECAR/Diagnostics/Viewer-Image-Analysis/images/stability_FP2/*.tiff
+for file in /home/sara/Documents/SECAR/Diagnostics/Viewer-Image-Analysis/images/stability_FP2/cropped/*.tiff
 do 
 	 python ./src/im_analysis.py "$file"
 done
